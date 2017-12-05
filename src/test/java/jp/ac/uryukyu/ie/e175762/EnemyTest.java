35abf425890ef0c3d9484baeda46ad23df0759d7
Tomohiro Inoue <e175762@ie.u-ryukyu.ac.jp>
2017-12-05 15:18:42 +0900

    1st Commit

---
+++ src/test/java/jp/ac/uryukyu/ie/e175762/EnemyTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e175762;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+class EnemyTest {
+    @Test
+    void attack() {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.setDead(true);
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.getHitPoint());
+    }
+
+}
